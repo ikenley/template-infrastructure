4f2c01f3be4734b4735edf1d258dcdd480adbe61
ikenley <ikenley6@gmail.com>
2023-01-21 14:35:56 -0500

    two subnets is better than 3

--- dev/core/main.tf
+++ dev/core/main.tf
@@ -10,7 +10,7 @@ locals {
   domain_name = "ikenley.com"
 
   # Quick way to turn off expensive services
-  spend_money = true
+  spend_money = false
 }
 
 terraform {
@@ -55,10 +55,10 @@ module "core" {
   cidr          = "10.0.0.0/18"
   dns_server_ip = "10.0.0.2"
 
-  azs              = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets   = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
-  database_subnets = ["10.0.20.0/24", "10.0.21.0/24", "10.0.22.0/24"]
+  azs              = ["us-east-1a", "us-east-1b"]
+  private_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
+  public_subnets   = ["10.0.10.0/24", "10.0.11.0/24"]
+  database_subnets = ["10.0.20.0/24", "10.0.21.0/24"]
 
   vpc_client_cidr = "10.1.0.0/22"
 
