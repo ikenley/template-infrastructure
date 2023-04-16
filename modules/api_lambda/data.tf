f73434bca7117341a43d246e48dcd9fb96773d10
ikenley <ikenley6@gmail.com>
2023-04-16 12:25:35 -0400

    alb listener invokes lambda

--- modules/api_lambda/data.tf
+++ modules/api_lambda/data.tf
@@ -2,8 +2,19 @@ locals {
   core_output_prefix = "/${var.namespace}/${var.env}/core"
 }
 
+# Network
+
 data "aws_ssm_parameter" "alb_public_arn" {
   name  = "${local.core_output_prefix}/alb_public_arn"
+}
+
+data "aws_lb" "this" {
+  arn  = data.aws_ssm_parameter.alb_public_arn.value
+}
+
+data "aws_lb_listener" "prod" {
+  load_balancer_arn = data.aws_ssm_parameter.alb_public_arn.value
+  port              = 443
 }
 
 data "aws_ssm_parameter" "alb_public_sg_id" {
