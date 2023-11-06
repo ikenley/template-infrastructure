fbce51306a47fe67abe11f326753ad9a275e84b6
ikenley <ikenley6@gmail.com>
2023-11-06 06:40:12 -0500

    stash

--- dev/auth_service/main.tf
+++ dev/auth_service/main.tf
@@ -32,7 +32,7 @@ module "auth_service" {
   project_name = "auth"
 
   git_repo   = "ikenley/auth-service"
-  git_branch = "login" # TODO revert to "main"
+  git_branch = "main"
 
   parent_domain_name = "ikenley.com"
   domain_name        = "auth-service.ikenley.com"
