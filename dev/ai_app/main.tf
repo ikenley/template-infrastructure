a7fee8ad108baf2f877eb3215737c072df3fe5b2
ikenley <ikenley6@gmail.com>
2023-11-28 06:29:57 -0500

    ai-app image generation

--- dev/ai_app/main.tf
+++ dev/ai_app/main.tf
@@ -36,7 +36,7 @@ module "ai_app" {
   project_name = "ai"
 
   git_repo   = "ikenley/ai-app"
-  git_branch = "main"
+  git_branch = "image" #"main"
 
   parent_domain_name = "ikenley.com"
   domain_name        = "ai.ikenley.com"
