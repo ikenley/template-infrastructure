a137732c249f28038d5b8e9d6454648dad04d2c6
ikenley <ikenley6@gmail.com>
2024-04-20 07:26:43 -0400

    gateway works

---
+++ modules/api_lambda/api_gateway.tf
@@ -0,0 +1,158 @@
+#------------------------------------------------------------------------------
+# DNS and API Gateway
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# API Gateway
+#------------------------------------------------------------------------------
+
+resource "aws_apigatewayv2_api" "this" {
+  name          = local.id
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "$default"
+
+  target = "integrations/${aws_apigatewayv2_integration.this.id}"
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  api_id           = aws_apigatewayv2_api.this.id
+  integration_type = "AWS_PROXY"
+
+  payload_format_version = "2.0"
+  connection_type        = "INTERNET"
+  description            = "Invoke Lambda API service"
+  integration_uri        = aws_lambda_function.this.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_deployment" "this" {
+  api_id      = aws_apigatewayv2_api.this.id
+  description = "Default deployment"
+
+  triggers = {
+    redeployment = sha1(join(",", tolist([
+      jsonencode(aws_apigatewayv2_integration.this),
+      jsonencode(aws_apigatewayv2_route.default),
+    ])))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#------------------------------------------------------------------------------
+# DNS record
+#------------------------------------------------------------------------------
+
+# data "aws_route53_zone" "this" {
+#   name         = "${var.parent_domain_name}."
+#   private_zone = false
+# }
+
+# resource "aws_route53_record" "this" {
+#   zone_id         = data.aws_route53_zone.this.zone_id
+#   name            = var.domain_name
+#   allow_overwrite = true
+#   type            = "A"
+
+#   alias {
+#     name                   = data.aws_lb.this.dns_name
+#     zone_id                = data.aws_lb.this.zone_id
+#     evaluate_target_health = false
+#   }
+# }
+
+# resource "aws_acm_certificate" "this" {
+#   domain_name       = var.domain_name
+#   validation_method = "DNS"
+# }
+
+# resource "aws_route53_record" "validation" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
+
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = data.aws_route53_zone.this.zone_id
+# }
+
+# resource "aws_acm_certificate_validation" "this" {
+#   certificate_arn         = aws_acm_certificate.this.arn
+#   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+# }
+
+
+
+# #------------------------------------------------------------------------------
+# # Lambda security group
+# #------------------------------------------------------------------------------
+
+# resource "aws_security_group" "api_lambda" {
+#   name        = "${local.id}-lambda"
+#   description = "Allow inbound traffic from ALB and outbound to all"
+#   vpc_id      = data.aws_ssm_parameter.vpc_id.value
+# }
+
+# resource "aws_security_group_rule" "api_lambda_ingress" {
+#   security_group_id = aws_security_group.api_lambda.id
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+
+#   source_security_group_id = data.aws_ssm_parameter.alb_public_sg_id.value
+# }
+
+# resource "aws_security_group_rule" "api_lambda_egress_http" {
+#   security_group_id = aws_security_group.api_lambda.id
+#   type              = "egress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
+
+# resource "aws_security_group_rule" "api_lambda_egress_https" {
+#   security_group_id = aws_security_group.api_lambda.id
+#   type              = "egress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
+
+# resource "aws_security_group_rule" "api_lambda_egress_pg" {
+#   security_group_id = aws_security_group.api_lambda.id
+#   type              = "egress"
+#   from_port         = 5432
+#   to_port           = 5432
+#   protocol          = "tcp"
+#   cidr_blocks       = [data.aws_ssm_parameter.vpc_cidr.value]
+# }
