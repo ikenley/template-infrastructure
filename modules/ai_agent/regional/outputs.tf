97fe487bf2107bb9d57fe39a996b7517df696563
ikenley <ikenley6@gmail.com>
2024-09-16 20:16:46 -0400

    agent created

--- modules/ai_agent/regional/outputs.tf
+++ modules/ai_agent/regional/outputs.tf
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# todo.tf
+# iam.tf
 #------------------------------------------------------------------------------
 
 resource "aws_ssm_parameter" "agent_resource_role_arn" {
@@ -8,8 +8,40 @@ resource "aws_ssm_parameter" "agent_resource_role_arn" {
   value = local.agent_resource_role_arn
 }
 
+output "agent_resource_role_arn" {
+  value = local.agent_resource_role_arn
+}
+
 resource "aws_ssm_parameter" "agent_resource_role_name" {
   name  = "${local.output_prefix}/agent_resource_role_name"
   type  = "String"
   value = local.agent_resource_role_name
+}
+
+output "agent_resource_role_name" {
+  value = local.agent_resource_role_name
+}
+
+#------------------------------------------------------------------------------
+# bedrock.tf
+#------------------------------------------------------------------------------
+
+resource "aws_ssm_parameter" "agent_arn" {
+  name  = "${local.output_prefix}/agent_arn"
+  type  = "String"
+  value = aws_bedrockagent_agent.this.arn
+}
+
+output "agent_arn" {
+  value = aws_bedrockagent_agent.this.arn
+}
+
+resource "aws_ssm_parameter" "agent_id" {
+  name  = "${local.output_prefix}/agent_id"
+  type  = "String"
+  value = aws_bedrockagent_agent.this.id
+}
+
+output "agent_id" {
+  value = aws_bedrockagent_agent.this.id
 }
