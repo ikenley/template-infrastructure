fd88af27b3fa12f9fa4786c8a4251f361813b1fd
ikenley <ikenley6@gmail.com>
2026-03-04 18:09:36 -0500

    memory-game (#50)
    * memory-game
    
    * source version default
    
    * create_index_html_function

---
+++ modules/game/frontend.tf
@@ -0,0 +1,54 @@
+# ------------------------------------------------------------------------------
+# Front-end: Static React application on S3 behind Cloudfront CDN
+# ------------------------------------------------------------------------------
+
+module "frontend" {
+  source = "../static_s3_website"
+
+  namespace    = var.namespace
+  env          = var.env
+  is_prod      = var.is_prod
+  project_name = var.project_name
+
+  parent_domain_name     = var.parent_domain_name
+  domain_name            = var.domain_name
+  create_acm_certificate = false
+  acm_certificate_arn    = module.acm_certificate.certificate_arn
+
+  path_prefix = "game/"
+
+  logs_bucket_name = data.aws_ssm_parameter.logs_s3_bucket_name.value
+
+  create_index_html_function = true
+
+  # Configure API Gateway origin
+  # additional_origins = {
+  #   "api-gateway" = {
+  #     domain_name  = replace(module.api_lambda.api_gateway_api_endpoint, "https://", "")
+  #     path_pattern = "/ai/api/*"
+  #   }
+  # }
+
+  tags = var.tags
+}
+
+
+# ACM certificate
+data "aws_route53_zone" "this" {
+  name         = "${var.parent_domain_name}."
+  private_zone = false
+}
+
+module "acm_certificate" {
+  source = "../acm_certificate"
+
+  namespace    = var.namespace
+  env          = var.env
+  is_prod      = var.is_prod
+  project_name = var.project_name
+
+  domain_name      = var.domain_name
+  route_53_zone_id = data.aws_route53_zone.this.zone_id
+
+  tags = var.tags
+}
---
+++ modules/game/main.tf
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------
+# Sanbox AI application. 
+# Also a demo of an cheap full-stack application
+# It will likely involve:
+# - Data layer: Existing reserved RDS Postgres instance
+# - API layer: Express.js hosted inside a Lambda function behind an ALB
+# - Front-end: Static React application on S3 behind Cloudfront CDN
+# ------------------------------------------------------------------------------
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+
+  id            = "${var.namespace}-${var.env}-game"
+  output_prefix = "/${var.namespace}/${var.env}/game"
+
+  git_repo = "https://github.com/ikenley/game"
+
+  tags = merge(var.tags, {
+    Terraform   = true
+    Environment = var.env
+    Namespace   = var.namespace
+    is_prod     = var.is_prod
+  })
+}
+
