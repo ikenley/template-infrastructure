184a1a51201cb3fbd0fdc0fe21a8cb65b4728251
ikenley <ikenley6@gmail.com>
2026-06-07 15:16:42 -0400

    prediction codebuild standard:5.0 (#56)

--- modules/prediction_app/cicd.tf
+++ modules/prediction_app/cicd.tf
@@ -147,7 +147,7 @@ resource "aws_codebuild_project" "codebuild_main" {
   environment {
     type                        = "LINUX_CONTAINER"
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     image_pull_credentials_type = "CODEBUILD"
     privileged_mode             = true
 
